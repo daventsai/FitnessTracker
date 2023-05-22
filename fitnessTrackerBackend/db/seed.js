1ede39efc3b9891618cdcddee1d503021afe4785
daventsai <daventsai75@gmail.com>
2023-05-22 11:15:56 -0500

    db seed setup except populate table

--- fitnessTrackerBackend/db/seed.js
+++ fitnessTrackerBackend/db/seed.js
@@ -8,14 +8,68 @@ const{
 
 async function dropTables(){
     //Drops all tables, ORDER MATTERS
+    try {
+        console.log("Starting to drop tables...");
+        await client.query(`
+            DROP TABLE IF EXISTS routine_activities;
+            DROP TABLE IF EXISTS activities;
+            DROP TABLE IF EXISTS routines;
+            DROP TABLE IF EXISTS users;
+        `);
+    } catch (error) {
+        console.error("Error dropping tables");
+        throw error;
+    }
 }
 
 async function createTables(){
     //Define the table
+    try {
+        console.log("Starting to create tables...");
+        await client.query(`
+            CREATE TABLE users(
+                id SERIAL PRIMARY KEY,
+                username varchar(255) UNIQUE NOT NULL,
+                password varchar(255) NOT NULL
+            );
+            CREATE TABLE routines(
+                id SERIAL PRIMARY KEY,
+                creator_id INTEGER REFERENCES users(id),
+                is_public BOOLEAN DEFAULT false,
+                name varchar(255) UNIQUE NOT NULL,
+                goal varchar(255) NOT NULL
+            );
+            CREATE TABLE activities(
+                id SERIAL PRIMARY KEY,
+                name varchar(255) UNIQUE NOT NULL,
+                description varchar(255) NOT NULL
+            );
+            CREATE TABLE routine_activities(
+                id SERIAL PRIMARY KEY,
+                routine_id INTEGER REFERENCES routines(id),
+                activity_id INTEGERY REFERENCES activities(id),
+                count INTEGER,
+                duration INTEGER
+            );
+        `);
+    } catch (error) {
+        console.error("Error creating tables");
+        throw error;
+    }
+
 }
 
 async function populateTables(){
     //Seed tables w/ dummy data from seedData.js
+    try{
+        console.log("Starting to populate tables...");
+
+
+    }
+    catch(error){
+        console.error("Error populating tables");
+        throw error;
+    }
 }
 
 async function rebuildDB(){
