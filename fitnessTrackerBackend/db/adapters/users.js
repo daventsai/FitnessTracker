282c04de05f10958e231d7554fa4b007af1bb9a8
daventsai <daventsai75@gmail.com>
2023-05-24 09:40:06 -0500

    hashed password on creating user

--- fitnessTrackerBackend/db/adapters/users.js
+++ fitnessTrackerBackend/db/adapters/users.js
@@ -1,13 +1,17 @@
-const client = require("../client");
+const client = require('../client');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 async function createUser({username,password}){
     try {
+        const hashedPassword = await bcrypt.hash(password.toString(),SALT_ROUNDS);
+        console.log("hashedPassword being set: ", hashedPassword);
         const {rows: [user]} = await client.query(`
             INSERT INTO users(username,password)
             VALUES($1,$2)
             ON CONFLICT (username) DO NOTHING
-            RETURNING *;
-            `,[username,password]);
+            RETURNING username;
+            `,[username,hashedPassword]);
         return user;
     } catch (error) {
         console.error('Error creating users');
@@ -15,5 +19,8 @@ async function createUser({username,password}){
     }
 }
 
+async function getUser({username,password}){
+
+}
 
 module.exports = {createUser};
