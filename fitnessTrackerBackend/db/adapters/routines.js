283159b6db34c2974fa4c8387e02abff4ffbc804
daventsai <daventsai75@gmail.com>
2023-05-24 11:29:29 -0500

    working on routines

--- fitnessTrackerBackend/db/adapters/routines.js
+++ fitnessTrackerBackend/db/adapters/routines.js
@@ -1,18 +1,22 @@
 const client = require("../client");
 
-async function createRoutine({creatorId,isPublic,name,goal}){
+async function createRoutine({creator_id,is_public,name,goal}){
     try {
         const { rows: [routine]} = await client.query(`
-            INSERT INTO routines("creatorId","isPublic",name,goal)
+            INSERT INTO routines(creator_id,is_public,name,goal)
             VALUES($1,$2,$3,$4)
             ON CONFLICT (name) DO NOTHING
             RETURNING *;
-        `,[creatorId,isPublic,name,goal]);
+        `,[creator_id,is_public,name,goal]);
         return routine;
     } catch (error) {
         console.error('Error creating routines');
         throw error;
     }
+}
+
+async function getRoutineById(routineId){
+    
 }
 
 module.exports = {createRoutine};
