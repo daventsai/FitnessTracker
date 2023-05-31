c5f6c45c40586d9687588f64c6b47c50f9a80b32
daventsai <daventsai75@gmail.com>
2023-05-30 21:06:22 -0500

    fixed delete, except the await destroyRoutine doesn't finish running

--- fitnessTrackerBackend/routes/routines.js
+++ fitnessTrackerBackend/routes/routines.js
@@ -17,14 +17,8 @@ routinesRouter.get('/',async(req,res,next)=>{
 
 routinesRouter.post('/',authRequired,async(req,res,next)=>{
     try {
-        const {token} = req.signedCookies;
-        const {is_public,name,goal} = req.body;
-        const parsedToken = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-        const userId = parsedToken.id;
-        console.log("parsedToken info: ", userId);
-
-        //-----------------NOT SAVING CREATOR_ID----------------------------
-        const routine = await createRoutine({userId,is_public,name,goal});
+        const {creator_id,is_public,name,goal} = req.body;
+        const routine = await createRoutine({creator_id,is_public,name,goal});
         console.log("routine: ", routine);
         res.send({
             message: 'Posting Routine Successful',
@@ -39,13 +33,15 @@ routinesRouter.post('/',authRequired,async(req,res,next)=>{
 routinesRouter.delete('/:routineId',authRequired,async(req,res,next)=>{
     try {
         const {token} = req.signedCookies;
-        const {routine_id} = req.body;
+        const {routineId} = req.params;
         const parsedToken = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
         const userId = parsedToken.id;
-        const creatorId = await getRoutineById(routine_id).creator_id;
+        const routine = await getRoutineById(routineId);
+        const creatorId = routine.creator_id;
+        console.log('routineObj',await getRoutineById(routineId));
         console.log('user id: '+ userId + ' | creatorId: ' + creatorId);
         if (userId === creatorId){
-            await destroyRoutine(routine_id);
+            await destroyRoutine(routineId);
         }
         else{
             next({
