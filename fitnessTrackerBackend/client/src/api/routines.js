9f32f36447065c282c4b488bbc9a023393af1e34
daventsai <daventsai75@gmail.com>
2023-06-10 17:49:55 -0500

    barely works

--- fitnessTrackerBackend/client/src/api/routines.js
+++ fitnessTrackerBackend/client/src/api/routines.js
@@ -26,3 +26,23 @@ export async function fetchRoutines(){
         console.log('Error getting my routines: ', error)
     }
 }
+
+export async function createRoutine(is_public,name,goal){
+    try {
+        const response = await fetch('/api/routines',{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                is_public,
+                name,
+                goal
+            })
+        });
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log('Error creating activities: ', error)
+    }
+}
--- fitnessTrackerBackend/client/src/components/MyRoutines.jsx
+++ fitnessTrackerBackend/client/src/components/MyRoutines.jsx
@@ -1,7 +1,7 @@
 import { useEffect,useState } from "react";
 import useAuth from "../hooks/useAuth"
 import { useNavigate } from "react-router-dom"
-import { fetchRoutines } from "../api/routines";
+import { fetchRoutines,createRoutine } from "../api/routines";
 import Header from "./Header";
 
 export default function MyRoutines(){
@@ -9,20 +9,45 @@ export default function MyRoutines(){
     const{loggedIn,user} = useAuth();
     const[routines,setRoutines] = useState([]);
     const [routineName,setRoutineName] = useState('');
+    const [isPublic,setIsPublic] = useState(false);
+    const [name,setName] = useState('');
+    const [goal,setGoal] = useState('');
+    const [submitted,setSubmitted] = useState(false);
     useEffect(()=>{
         async function getMyRoutines(){
             if (loggedIn === true){
                 setRoutines(await fetchRoutines());
+                if (submitted){
+                    setSubmitted(false);
+                }
             }
             else{
                 nav('/');
             }
         }
         getMyRoutines();
-    },[])
+    },[submitted])
     let routineDisplay;
     if (routines.routines){
         routineDisplay = routineName.length ? routines.routines.filter((r)=>(r.creator_id===user.id && r.name.toLowerCase().includes(routineName.toLowerCase()))) : routines.routines.filter((r)=>(r.creator_id===user.id));
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        try {
+            const result = await createRoutine(isPublic,name,goal);
+            setName('');
+            setGoal('');
+            setIsPublic(false);
+            const x = document.getElementsByClassName("isPublic");
+            for(let i=0; i<x.length; i++) {
+                x[i].checked = false;
+                }   
+            setSubmitted(true);
+            console.log('create routine result',result);
+        } catch (error) {
+            console.log('Error on creating a routine',error);
+        }
     }
 
     return(
@@ -34,38 +59,53 @@ export default function MyRoutines(){
                 :
                 <div>
                     <Header/>
-                    <div className='search'>
-                        <p>Search for a routine: </p>
-                        <input type='text' onChange={(e)=>setRoutineName(e.target.value)}/>
-                    </div>
-                    <div>
-                    {
-                        typeof routines.routines !== 'undefined'
-                        ?
-                        <div>
-                            {routineDisplay.map((routine)=>{
-                                return(
-                                    <div style={{border: '3px solid white', margin: '10px'}}>
-                                        <h3>Name: {routine.name}</h3>
-                                        <p>Goal: {routine.goal}</p>
-                                        <p>Created By: {routine.creator_id}</p>
-                                        {routine.activities.map((activity)=>{
-                                            return(
-                                                <div style={{border: '1px dotted teal', margin: '3px'}}>
-                                                    <h5>Activity: {activity.name}</h5>
-                                                    <h6>Description: {activity.description}</h6>
-                                                    <h6>Duration:{activity.duration}</h6>
-                                                    <h6>Count: {activity.count}</h6>
-                                                </div>
-                                            )
-                                        })}
-                                    </div>
-                                )
-                            })}
+                    <div className='myRoutines'>
+                        <div className='routines'>
+                            <div className='search'>
+                                <p>Search for a routine: </p>
+                                <input type='text' onChange={(e)=>setRoutineName(e.target.value)}/>
+                            </div>
+                            <div>
+                            {
+                                typeof routines.routines !== 'undefined'
+                                ?
+                                <div>
+                                    {routineDisplay.map((routine)=>{
+                                        return(
+                                            <div style={{border: '3px solid white', margin: '5px'}}>
+                                                <h3>Name: {routine.name}</h3>
+                                                <p>Goal: {routine.goal}</p>
+                                                <p>Created By: {routine.creator_id}</p>
+                                                {routine.activities.map((activity)=>{
+                                                    return(
+                                                        <div style={{border: '1px dotted teal', margin: '2px'}}>
+                                                            <h5>Activity: {activity.name}</h5>
+                                                            <h6>Description: {activity.description}</h6>
+                                                            <h6>Duration:{activity.duration}</h6>
+                                                            <h6>Count: {activity.count}</h6>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                :
+                                <div/>
+                            }
+                            </div>
                         </div>
-                        :
-                        <div/>
-                    }
+                        <div style={{border:'3px solid white',margin:'5px'}}>
+                            <h2>Create new Routine</h2>
+                            <div>
+                                <form onSubmit={handleSubmit}>
+                                    <p><input type='text' name='name' placeholder='routine name' value={name} onChange={(e)=>setName(e.target.value)}/></p>
+                                    <p><input type='text' name='goal' placeholder='goal' value={goal} onChange={(e)=>setGoal(e.target.value)}/></p>
+                                    <p>Public?<input type='checkbox' className='isPublic' name='isPublic' onChange={(e)=>setIsPublic(e.target.checked?true:false)}/></p>
+                                    <button>Submit</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             }
